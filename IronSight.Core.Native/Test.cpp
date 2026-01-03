166814d99726a2c50e7a90bac2a1f063e605143d
Bin Idealend <q34235212543452345@outlook.com>
2026-01-04 00:03:12 +0800

    Placeholder

---
+++ IronSight.Core.Native/Test.cpp
@@ -0,0 +1,14 @@
+﻿#include <pch.h>
+#include "Utilities.h"
+
+
+namespace IronSight::Core::Native
+{
+	class Test
+	{
+		void Placeholder()
+		{
+			LOG_TRACE("THIS IS THE PLACEHOLDER FUNCTION");
+		}
+	};
+}
